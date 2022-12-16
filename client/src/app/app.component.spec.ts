d389142958bb72cdb19918dda0b90d678b2e3233
Georgi Petrov <hailanimepenchou@gmail.com>
2022-12-16 22:33:21 +0200

    major bump angular to ^15

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-project-final'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-project-final');
-  });
+//   it(`should have as title 'client'`, () => {
+//     const fixture = TestBed.createComponent(AppComponent);
+//     const app = fixture.componentInstance;
+//     expect(app.title).toEqual('client');
+//   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-project-final app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('client app is running!');
   });
 });
